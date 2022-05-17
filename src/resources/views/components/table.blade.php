f9a6ed9557dff9b88aebf6035ae57d4de71b1dcd
Michael K. Ocansey <kabutey@gmail.com>
2022-05-17 15:12:56 +0000

    render component/table better on mobile

--- src/resources/views/components/table.blade.php
+++ src/resources/views/components/table.blade.php
@@ -9,9 +9,13 @@
     'hover_effect' => 'true',
 ])
 
-<table class="bw-table w-full @if($has_shadow == 'true') shadow-2xl shadow-gray-200 @endif  @if($divided == 'true') divided @if($divider=='thin') thin @endif @endif  @if($striped == 'true') striped @endif @if($hover_effect=='true') with-hover-effect @endif">
-    <thead>
-        <tr class="text-gray-500 bg-gray-200">{{ $header }}</tr>
-    </thead>
-    <tbody>{{ $slot }}</tbody>
-</table>
+<div class="max-w-screen overflow-x-hidden md:w-full">
+    <div class="w-full overflow-x-scroll">
+        <table class="bw-table w-full @if($has_shadow == 'true') shadow-2xl shadow-gray-200 @endif  @if($divided == 'true') divided @if($divider=='thin') thin @endif @endif  @if($striped == 'true') striped @endif @if($hover_effect=='true') with-hover-effect @endif">
+            <thead>
+                <tr class="text-gray-500 bg-gray-200">{{ $header }}</tr>
+            </thead>
+            <tbody>{{ $slot }}</tbody>
+        </table>
+    </div>
+</div>
