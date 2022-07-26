6b1294702b4f4be186c069d041a3dc20b7bdf39d
Michael K. Ocansey <kabutey@gmail.com>
2022-07-26 11:42:46 -0500

    Added component/timeline

--- src/resources/views/components/empty-state.blade.php
+++ src/resources/views/components/empty-state.blade.php
@@ -15,13 +15,13 @@
     $button_label = $buttonLabel;
 @endphp
 <div class="text-center px-4 pb-10 bw-empty-state {{$class}}">
-    @if($show_image == 'true')<img src="{{ $image }}" class="h-52 mx-auto mb-3" />@endif
-    @if($heading != '')<div class="text-gray-600 text-xl pt-3 px-4 font-light">{!!$heading!!}</div>@endif
-    @if($message != '')<div class="text-slate-600/70 text-sm pt-2 px-6">{!!$message!!}</div>@endif
+    @if($show_image == 'true')<img src="{{ $image }}" class="h-52 mx-auto mb-6" />@endif
+    @if($heading != '')<div class="text-slate-700 text-2xl pt-4 pb-3 px-4 font-light">{!!$heading!!}</div>@endif
+    @if($message != '')<div class="text-slate-600/70 px-6">{!!$message!!}</div>@endif
     <div class="pt-2">{!! $slot !!}</div>
     @if($button_label != '')
         <x-bladewind::button 
             onclick="{!!$onclick!!}" class="block mx-auto my-4"
-            size="tiny">{{$button_label}}</x-bladewind::button>
+            size="small">{{$button_label}}</x-bladewind::button>
     @endif
 </div>
---
+++ src/resources/views/components/timeline.blade.php
@@ -0,0 +1,55 @@
+@props([
+    'status' => 'pending',
+    'stacked' => 'false',
+    'date' => '',
+    'label' => '',
+    'last' => 'false',
+    'color' => 'cyan',
+    'coloring' => [
+        'bg' => [
+            'red' => 'bg-red-500',
+            'yellow' => 'bg-yellow-500',
+            'green' => 'bg-emerald-500',
+            'blue' => 'bg-blue-500',
+            'orange' => 'bg-orange-500',
+            'purple' => 'bg-purple-500',
+            'cyan' => 'bg-cyan-500',
+            'pink' => 'bg-pink-500',
+            'black' => 'bg-black',
+        ],
+        'border' => [
+            'red' => 'border-red-500',
+            'yellow' => 'border-yellow-500',
+            'green' => 'border-emerald-500',
+            'blue' => 'border-blue-500',
+            'orange' => 'border-orange-500',
+            'purple' => 'border-purple-500',
+            'cyan' => 'border-cyan-500',
+            'pink' => 'border-pink-500',
+            'black' => 'border-black',
+        ],
+        'text' => [
+            'red' => 'text-red-500',
+            'yellow' => 'text-yellow-500',
+            'green' => 'text-emerald-500',
+            'blue' => 'text-blue-500',
+            'orange' => 'text-orange-500',
+            'purple' => 'text-purple-500',
+            'cyan' => 'text-cyan-500',
+            'pink' => 'text-pink-500',
+            'black' => 'text-black',
+        ],
+    ],
+])
+<div class="flex text-slate-600">
+    @if($stacked !== 'true')
+    <div class="pr-5 pt-1 w-[63px]  font-semibold whitespace-nowrap {{ $coloring['text'][$color] }}">{!!$date!!}</div>
+    @endif
+    <div class="z-20">
+        <div class="h-8 w-8 @if($status=='pending') bg-white border-4 {{ $coloring['border'][$color] }}  @else {{$coloring['bg'][$color]}} @endif rounded-full"></div>
+    </div>
+    <div class="@if($last!='true') border-l-4 {{ $coloring['border'][$color] }}@endif -ml-[16px] pl-8 pb-14 z-10 text-lg">
+        @if($stacked == 'true') <div class="font-semibold {{ $coloring['text'][$color] }}">{!!$date!!}</div> @endif
+        {!!$label!!}
+    </div>
+</div>
