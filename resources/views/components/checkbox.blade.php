3d5cd872ee6c5073e7f19142df9a4df805eadf8d
Ngô Quốc Đạt <datlechin@gmail.com>
2022-12-18 00:09:45 +0700

    Refactor code 2

--- resources/views/components/checkbox.blade.php
+++ resources/views/components/checkbox.blade.php
@@ -1,24 +1,22 @@
-@props([ 
+@props([
     'name' => 'checkbox',
-    'value' => '',
-    'label' => '',
-    'checked' => 'false',
-    'disabled' => 'false',
+    'value' => null,
+    'label' => null,
+    'checked' => false,
+    'disabled' => false,
     'type' => 'checkbox',
-    'css' => '',
+    'css' => null,
 ])
-@php $name = preg_replace('/[\s-]/', '_', $name); @endphp
+@php($name = preg_replace('/[\s-]/', '_', $name))
 
-<label 
-    class="inline-flex items-center cursor-pointer text-sm @if($disabled=='true') opacity-60 @endif {{$css}}">
-    <input 
-        value="{{$value}}" 
-        name="{{$name}}"
-        @if($disabled=='true') disabled @endif
-        class="text-blue-500 
-            w-5 h-5 mr-2 disabled:opacity-60
-            focus:ring-blue-400 focus:ring-opacity-25 
-            border border-gray-300 bw-checkbox 
-            rounded" 
-        type="{{$type}}" @if($checked == 'true') checked @endif />{!! $label !!}
-</label>  &nbsp; &nbsp;
+<label class="inline-flex items-center cursor-pointer text-sm @if($disabled) opacity-60 @endif {{ $css }}">
+    <input
+        type="{{ $type }}"
+        name="{{ $name }}"
+        class="text-blue-500 w-5 h-5 mr-2 disabled:opacity-60 focus:ring-blue-400 focus:ring-opacity-25 border border-gray-300 bw-checkbox rounded"
+        @if($disabled) disabled @endif
+        @if($checked) checked @endif
+        value="{{ $value }}"
+    />
+    {!! $label !!}
+</label>
--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -8,9 +8,9 @@
     'placeholder' => 'Select One',
 
     // optional function to execute when a dropdown item is selected
-    // by default the value of a dropdown item is wriiten to an input field with the 
+    // by default the value of a dropdown item is wriiten to an input field with the
     // name dd_name. Where name is the name you provided for the dropdown
-    // if you named your dropdown countries for example, whatever country is selected can 
+    // if you named your dropdown countries for example, whatever country is selected can
     // be found in the <input type="hidden" clas="input-countries" name="dd_countries" />
     'onselect' => '',
 
@@ -19,36 +19,36 @@
     // value is whatever value will be passed to your code when an item is selected
     // label is what will be displayed to the user
     // if you want to display icons for each item your json can contain the optional 'icon' key
-    // where icons are required, they must be in the semantic UI icon format 
+    // where icons are required, they must be in the semantic UI icon format
     // [{"label":"Burkina Faso","icon":"bf flag","value":"+226"},{"label":"Ghana","icon":"gh flag","value":"+233"},{"label":"Ivory Coast","icon":"ivc flag","value":"+228"}]
     'data' => [],
 
     // what key in your data array should be used to populate 'value' of the dropdown when an item is selected
-    // by default a key of 'value' is used. If your data is something like [ {"id":"1","name":"Burkina Faso"}] 
+    // by default a key of 'value' is used. If your data is something like [ {"id":"1","name":"Burkina Faso"}]
     // your value_key will be 'id'
     'value_key' => 'value',
     'valueKey' => 'value',
 
     // what key in your data array should be used to display the labels the user will see as dropdown items
-    // the default key used for labels is 'label'. If your data is something like [ {"id":"1","name":"Burkina Faso"}] 
+    // the default key used for labels is 'label'. If your data is something like [ {"id":"1","name":"Burkina Faso"}]
     // your label_key will be 'name'
     'label_key' => 'label',
     'labelKey' => 'label',
 
     // what key in your data array should be used to display flag icons next to the labels
-    // [ {"id":"1","name":"Burkina Faso", "flag":"/assets/images/bf-flag.png"}] 
+    // [ {"id":"1","name":"Burkina Faso", "flag":"/assets/images/bf-flag.png"}]
     // your flag_key will be 'image'
     'flag_key' => '',
     'flagKey' => '',
-    
+
     // what key in your data array should be used to display images next to the labels
-    // the default key used for images is '', meaning images will be ignored. If your data is something like 
-    // [ {"id":"1","name":"Burkina Faso", "image":"/assets/images/bf-flag.png"}] 
+    // the default key used for images is '', meaning images will be ignored. If your data is something like
+    // [ {"id":"1","name":"Burkina Faso", "image":"/assets/images/bf-flag.png"}]
     // your image_key will be 'image'
     'image_key' => '',
     'imageKey' => '',
-    
-    // there are times you will want the dropdown items to go to a link when clicked on 
+
+    // there are times you will want the dropdown items to go to a link when clicked on
     // useful if you are using the dropdown as a navigation component for example
     // the url_key defines which key in your data array to be use as urls
     // the default key used for urls is '', meaning urls will be ignored.
@@ -60,15 +60,15 @@
     'append_value_to_url' => 'false',
     'appendValueToUrl' => 'false',
 
-    // if url_key is set and append_value_to_url is 'true', what variable name should 
+    // if url_key is set and append_value_to_url is 'true', what variable name should
     // the value be appended to the url as. Default is 'value'
     // url will look like /user/settings/?value=devices
     'append_value_to_url_as' => 'value',
     'appendValueToUrlAs' => 'value',
 
-    // there are instances you want the name passed by the dropdown when you submit a form to be 
-    // different from the name you gave the dropdown. Example. you may name the dropdown as country but 
-    // want it to submit data as country_id. 
+    // there are instances you want the name passed by the dropdown when you submit a form to be
+    // different from the name you gave the dropdown. Example. you may name the dropdown as country but
+    // want it to submit data as country_id.
     'data_serialize_as' => '',
     'dataSerializeAs' => '',
 
@@ -77,15 +77,15 @@
 
     // adds margin after the input box
     'add_clearing' => 'true',
-    'addClearing' => 'true', 
-    
+    'addClearing' => 'true',
+
     // determines if a value passed in the data array should automatically be selected
     // useful when using the component in edit mode or as part of filter options
     // the value you specify should exist in your value_key. If your value_key is 'id', you
     // cannot set a selected_value of 'maize white'
     'selected_value' => '',
     'selectedValue' => '',
-    
+
     // setting this to true adds a search box above the dropdown items
     // this can be used to filter the contents of the dropdown items
     'searchable' => 'false',
@@ -95,7 +95,7 @@
     'show_filter_icon' => 'false',
     'showFilterIcon' => 'false',
 ])
-@php 
+@php
     // reset variables for Laravel 8 support
     $value_key = $valueKey;
     $label_key = $labelKey;
@@ -116,7 +116,7 @@
 
     if(! isset($data[0]->{$label_key}) ) {
         echo '<p style="color:red">
-            &lt;x-bladewind.dropdown /&gt;: ensure the value you set as label_key 
+            &lt;x-bladewind.dropdown /&gt;: ensure the value you set as label_key
             exists in your array data</p>';exit;
     }
     if( $url_key !== '' && ! isset($data[0]->{$url_key}) ) {
@@ -144,9 +144,9 @@
             <path fill-rule="evenodd" d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd" />
         </svg>
     </button>
-    <div class="w-full absolute z-50 bg-white -mt-1 shadow-md cursor-pointer hidden dropdown-items-parent dark:text-white dark:border-slate-700 dark:bg-slate-600" 
+    <div class="w-full absolute z-50 bg-white -mt-1 shadow-md cursor-pointer hidden dropdown-items-parent dark:text-white dark:border-slate-700 dark:bg-slate-600"
         style="max-height: 270px; overflow: scroll;">
-        <div 
+        <div
             class="dropdown-items border border-gray-300 divide-y relative w-full">
             @if($searchable == 'true')
                 <div class="bg-gray-100 dark:bg-slate-500 p-2 sticky top-0 min-w-full">
@@ -158,18 +158,18 @@
                         placeholder="Search" />
                 </div>
             @endif
-            <div 
-                data-value="" 
-                data-label="" 
+            <div
+                data-value=""
+                data-label=""
                 data-user-function=""
-                data-parent="{{ $name }}" 
+                data-parent="{{ $name }}"
                 class="dd-item p-4 cursor-pointer default hidden">{{ $placeholder }}
                 @if($required == 'true') &nbsp;<span class="text-red-300">*</span>@endif</div>
             @for ($x=0; $x < count($data); $x++)
                 @php
                     $url_target = 'self';
-                    $url = (isset($data[$x]->$url_key) && $data[$x]->$url_key !== '') ? 
-                        ($data[$x]->{$url_key} . (($append_value_to_url === 'true') ? 
+                    $url = (isset($data[$x]->$url_key) && $data[$x]->$url_key !== '') ?
+                        ($data[$x]->{$url_key} . (($append_value_to_url === 'true') ?
                         "?{$append_value_to_url_as}=" . $data[$x]->$value_key : '')) : '';
 
                     if($url !== '' && ( Str::contains($url, 'http://') || Str::contains($url, 'https://')) && !Str::contains($url, request()->getHost()) ){
@@ -177,7 +177,7 @@
                     }
                     $value = $data[$x]->$value_key;
                 @endphp
-                <div 
+                <div
                     {{ $attributes->merge(['class' => "dd-item p-3 cursor-pointer hover:bg-gray-100 flex items-center dark:text-white dark:border-slate-700 dark:bg-slate-600 dark:hover:bg-slate-700 dark:text-gray-300"]) }}
                     data-href="{{$url}}"
                     data-href-target="{{$url_target}}"
@@ -194,13 +194,13 @@
                     <div>{!! $data[$x]->$label_key !!}</div>
                 </div>
             @endfor
-            <input 
-                type="hidden" 
-                class="bw-{{ $name }} {{ ($required == 'true') ? ' required' : '' }}" 
+            <input
+                type="hidden"
+                class="bw-{{ $name }} {{ ($required == 'true') ? ' required' : '' }}"
                 name="{{ ($data_serialize_as != '') ? $data_serialize_as : $input_name }}" />
         </div>
     </div>
 </div>
 
 <script>el_name = '{{ $name }}';</script>
-<script src="{{ asset('bladewind/js/dropdown.js') }}"></script>
+<script src="{{ asset('vendor/bladewind/js/dropdown.js') }}"></script>
