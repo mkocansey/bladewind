4fc744709da5cadcdc4bb658ae6abecafd3aaaea
Michael K. Ocansey <kabutey@gmail.com>
2023-06-11 18:00:38 +0000

    Update icon.blade.php

--- resources/views/components/icon.blade.php
+++ resources/views/components/icon.blade.php
@@ -6,11 +6,13 @@
     'type' => 'outline',
     // css classes to append to the svg file
     'class' => '',
+    // specify directory to load icons from
+    'dir' => ''
 ])
 @php 
-    $dir = (! in_array($type, [ 'outline', 'solid' ])) ? 'outline' : $type; 
-    $svg_file = file_exists(realpath("vendor/bladewind/icons/$dir/$name.svg")) ? 
-        realpath("vendor/bladewind/icons/$dir/$name.svg") : null;
+    $path = 'vendor/bladewind/icons';
+    $icons_dir = ($dir !== '') ? $dir : ((! in_array($type, [ 'outline', 'solid' ])) ? "{$path}/outline" : "$path/$type"); 
+    $svg_file = file_exists(realpath("$icons_dir/$name.svg")) ? realpath("$icons_dir/$name.svg") : null;
 @endphp
 @if (!empty($name))
     @if(substr($name, 0,4) === '<svg') {{-- do this for complete svg tags --}}
