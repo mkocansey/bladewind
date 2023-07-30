aab57219acb243f17d72d5512e0deb97f38ca060
Michael K. Ocansey <kabutey@gmail.com>
2023-07-30 23:11:02 +0000

    added air to tags vertically

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -35,9 +35,7 @@
     $rounded_class = ($rounded) ? 'rounded-full' : 'rounded-md';
     $clearing_css = ($add_clearing) ? 'mb-3' : '';
 @endphp
-<span class="bg-red-200 bg-yellow-200 bg-green-200 bg-pink-200 bg-cyan-200 bg-gray-200 bg-blue-200 bg-purple-200 bg-orange-200"></span>
-<span class="bg-red-300 bg-yellow-300 bg-green-300 bg-pink-300 bg-cyan-300 bg-gray-300 bg-blue-300 bg-purple-300 bg-orange-300 bg-red-500 bg-yellow-500 bg-green-500 bg-pink-500 bg-gray-500 bg-cyan-500 bg-blue-500 bg-gray-500 bg-purple-500 bg-orange-500 text-red-500 text-yellow-500 text-green-500 bg-gray-500 text-pink-500 text-cyan-500 text-purple-500 text-orange-500 text-red-50 text-yellow-50 text-green-50 text-pink-50 text-cyan-50 text-purple-50 text-gray-50 text-blue-50 text-orange-50 bg-red-200 bg-yellow-200 bg-green-200 bg-pink-200 bg-cyan-200 bg-gray-200 bg-blue-200 bg-purple-200 bg-orange-200 border-red-500 border-yellow-500 border-green-500 border-pink-500 border-gray-500 border-cyan-500 border-blue-500 border-gray-500 border-purple-500 border-orange-500"></span>
-<label id="@if($add_id_prefix)bw-@endif{{$id}}" class="relative text-[10px] uppercase px-[12px] tracking-widest whitespace-nowrap inline-block {{$rounded_class}} {{$clearing_css}} bg-{{$color}}-{{$color_weight[$shade]}} text-{{$color}}-{{$text_color_weight[$shade]}} {{$class}}">
+<label id="@if($add_id_prefix)bw-@endif{{$id}}" class="relative text-[10px] uppercase px-[12px] leading-8 tracking-widest whitespace-nowrap inline-block {{$rounded_class}} {{$clearing_css}} bg-{{$color}}-{{$color_weight[$shade]}} text-{{$color}}-{{$text_color_weight[$shade]}} {{$class}}">
 {{ $label }}
 @if($can_close)
 <a href="javascript:void(0)" onclick="@if($onclick=='')this.parentElement.remove()@else{!!$onclick!!}@endif">
@@ -47,3 +45,4 @@
 </a>
 @endif
 </label>
+{{--<span class="bg-red-200 bg-yellow-200 bg-green-200 bg-pink-200 bg-cyan-200 bg-gray-200 bg-blue-200 bg-purple-200 bg-orange-200"></span><span class="bg-red-300 bg-yellow-300 bg-green-300 bg-pink-300 bg-cyan-300 bg-gray-300 bg-blue-300 bg-purple-300 bg-orange-300 bg-red-500 bg-yellow-500 bg-green-500 bg-pink-500 bg-gray-500 bg-cyan-500 bg-blue-500 bg-gray-500 bg-purple-500 bg-orange-500 text-red-500 text-yellow-500 text-green-500 bg-gray-500 text-pink-500 text-cyan-500 text-purple-500 text-orange-500 text-red-50 text-yellow-50 text-green-50 text-pink-50 text-cyan-50 text-purple-50 text-gray-50 text-blue-50 text-orange-50 bg-red-200 bg-yellow-200 bg-green-200 bg-pink-200 bg-cyan-200 bg-gray-200 bg-blue-200 bg-purple-200 bg-orange-200 border-red-500 border-yellow-500 border-green-500 border-pink-500 border-gray-500 border-cyan-500 border-blue-500 border-gray-500 border-purple-500 border-orange-500"></span>--}}
