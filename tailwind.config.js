81ecbe2ec500b322f0b841a40416ff9ab3faf434
Michael K. Ocansey <kabutey@gmail.com>
2023-11-15 19:16:41 +0000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,29 @@
 const colors = require("tailwindcss/colors");
 module.exports = {
-  content: [
-    "./public/**/*.js",
-    "./resources/**/*.{html,php,js}", 
-  ],
-  safelist: [
-    'bg-slate-200',
-    'hover:bg-slate-300',
-  ],
-  darkMode: 'class',
-  theme: {
-    extend: {
-      colors: {
-        primary: colors.blue,
-        secondary: colors.slate,
-        green: colors.emerald,
-        dark: colors.slate,
-        success: colors.emerald,
-        error: colors.red,
-        warning: colors.amber,
-        info: colors.blue
-      }
+    content: [
+        "./public/**/*.js",
+        "./resources/**/*.{html,php,js}",
+    ],
+    safelist: [
+        'bg-slate-200',
+        'hover:bg-slate-300',
+    ],
+    darkMode: 'media',
+    theme: {
+        extend: {
+            colors: {
+                primary: colors.blue,
+                secondary: colors.slate,
+                green: colors.emerald,
+                dark: colors.slate,
+                success: colors.emerald,
+                error: colors.red,
+                warning: colors.amber,
+                info: colors.blue
+            }
+        },
     },
-  },
-  plugins: [
-      require('@tailwindcss/forms')
-  ],
+    plugins: [
+        require('@tailwindcss/forms')
+    ],
 };
