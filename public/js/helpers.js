1dc212b3ba97ae8abf04ddd6a99ff85dd49ea768
Michael K. Ocansey <kabutey@gmail.com>
2023-12-14 00:24:49 +0000

    Completed #151

--- public/js/helpers.js
+++ public/js/helpers.js
@@ -147,6 +147,8 @@ changeCss = (element, css, mode = 'add', elementIsDomObject = false) => {
 
 showModal = (element) => {
     unhide(`.bw-${element}-modal`);
+    document.body.classList.add('overflow-hidden');
+    dom_el(`.bw-${element}-modal`).focus();
     let current_index = (current_modal.length === 0) ? 0 : current_modal.length + 1;
     animateCSS(`.bw-${element}`, 'zoomIn').then(() => {
         current_modal[current_index] = element;
@@ -157,6 +159,8 @@ hideModal = (element) => {
     animateCSS(`.bw-${element}`, 'zoomOut').then(() => {
         hide(`.bw-${element}-modal`);
         current_modal.pop();
+        document.body.classList.remove('overflow-hidden');
+        dom_el(`.bw-${element}-modal`).removeEventListener('keydown', trapFocusInModal);
     });
 }
 
@@ -369,5 +373,23 @@ compareDates = (el1, el2, message, inline) => {
             }
         }
     }, 100);
+}
 
+
+trapFocusInModal = (event) => {
+    let modal_name = current_modal[(current_modal.length - 1)];
+    if (modal_name !== undefined) {
+        const focusableElements = dom_els(`.bw-${modal_name}-modal input:not([type='hidden']):not([class*='hidden']), .bw-${modal_name}-modal button:not([class*="hidden"]),  .bw-${modal_name}-modal a:not([class*="hidden"])`);
+        const firstElement = focusableElements[0];
+        const lastElement = focusableElements[focusableElements.length - 1];
+        if (event.key === 'Tab') {
+            if (event.shiftKey && document.activeElement === firstElement) {
+                event.preventDefault();
+                lastElement.focus();
+            } else if (!event.shiftKey && document.activeElement === lastElement) {
+                event.preventDefault();
+                firstElement.focus();
+            }
+        }
+    }
 }
