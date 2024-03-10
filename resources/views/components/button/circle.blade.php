8851b5ad3d7b31728d0018c614e9574428d1c8d5
Michael K. Ocansey <kabutey@gmail.com>
2024-03-10 17:26:33 +0000

    Fix: #195

--- resources/views/components/button/circle.blade.php
+++ resources/views/components/button/circle.blade.php
@@ -1,9 +1,10 @@
 @props([
-    'tooltip' => '',
-
+    'onclick' => '',
+    'title' => '',
 ])
 @aware([
     'type' => 'primary',
+    'color' => 'primary',
     'size' => 'regular',
     'name' => null,
     'can_submit' => false,
@@ -11,13 +12,21 @@
     'disabled' => false,
     'circular' => true,
     'tag' => 'button',
-    'color' => 'primary',
     'icon' => '',
     'radius' => 'full',
     'button_text_css' => null,
     'show_focus_ring' => true,
-    'tooltip' => '',
 ])
-<x-bladewind::button :type="$type" :color="$color" :size="$size" :name="$name" :can_submit="$can_submit"
-                     :disabled="$disabled" :show_focus_ring="$show_focus_ring" :tag="$tag" :icon="$icon"
-                     :button_text_css="$button_text_css" circular/>
+<x-bladewind::button
+        :color="$color"
+        :size="$size"
+        :name="$name"
+        :can_submit="$can_submit"
+        :disabled="$disabled"
+        :show_focus_ring="$show_focus_ring"
+        :tag="$tag"
+        :icon="$icon"
+        :button_text_css="$button_text_css"
+        :title="$title"
+        circular="true"
+        :onclick="$onclick"/>
