fe0be93ab88370287d3c60fca6efc5c1bede636e
Michael K. Ocansey <kabutey@gmail.com>
2024-04-14 18:47:16 +0000

    changed dark mode colours

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -20,6 +20,8 @@
     'footer' => null,
     // additional css
     'class' => null,
+    // the contact card uses this card component but needs to have a different class name
+    'is_contact_card' => false,
 ])
 @php
     // reset variables for Laravel 8 support
@@ -29,13 +31,19 @@
     $compact = filter_var($compact, FILTER_VALIDATE_BOOLEAN);
     $reducePadding = filter_var($reducePadding, FILTER_VALIDATE_BOOLEAN);
     $has_border = filter_var($has_border, FILTER_VALIDATE_BOOLEAN);
+    $is_contact_card = filter_var($is_contact_card, FILTER_VALIDATE_BOOLEAN);
     if ( !$hasShadow ) $has_shadow = $hasShadow;
     if ( $reducePadding ) {
         $reduce_padding = $reducePadding;
         $compact = $reduce_padding;
     }
 @endphp
-<div class="bw-card bg-white dark:bg-dark-900/50 @if($has_border) ring-1 ring-slate-800 ring-opacity-5 dark:ring-dark-700/50 focus:outline-none @endif @if($header === null && ! $compact) p-8 @elseif($compact) py-3 px-5 @else @endif rounded-md @if($has_shadow) shadow dark:shadow-dark-900/80 @if($hover_effect) hover:shadow-md cursor-pointer hover:!ring-opacity-15 hover:dark:ring-dark-300 @endif  @endif {{ $class }}">
+<div class="@if($is_contact_card) bw-contact-card @else bw-card @endif bg-white dark:bg-dark-800 rounded-md {{ $class }}
+@if($has_border) border border-slate-200 border-opacity-95 dark:border-dark-700/50 focus:outline-none @endif
+@if(!$header && ! $compact) p-8 @elseif($compact) py-3 px-5 @endif
+@if($has_shadow) shadow-md shadow-slate-200/50 dark:shadow-dark-900/50
+    @if($hover_effect) hover:shadow-md hover:dark:shadow-dark-900 cursor-pointer @endif
+@endif">
     @if($header)
         <div class="border-b border-gray-100/30 dark:border-dark-800/50">
             {{ $header }}
