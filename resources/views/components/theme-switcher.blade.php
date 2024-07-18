55dc6a5e2d65ac1fb8a4b1f65db56c2dfc4b06f0
Michael K. Ocansey <kabutey@gmail.com>
2024-07-18 02:39:22 +0000

    Fix: respect system theme if set by user

--- resources/views/components/theme-switcher.blade.php
+++ resources/views/components/theme-switcher.blade.php
@@ -33,11 +33,11 @@
                     dir="{{$iconDir}}"
                     class="text-primary-400 hover:text-primary-500 dark:!text-dark-500 dark:hover:!text-dark-400 stroke-2 theme-system hidden"/>
         </x-slot:trigger>
-        <x-bladewind::dropmenu-item onclick="chooseTheme('dark')" icon="{{$darkIcon}}" icon_css="stroke-2">
-            {{$darkText}}
-        </x-bladewind::dropmenu-item>
         <x-bladewind::dropmenu-item onclick="chooseTheme('light')" icon="{{$lightIcon}}" icon_css="stroke-2">
             {{$lightText}}
+        </x-bladewind::dropmenu-item>
+        <x-bladewind::dropmenu-item onclick="chooseTheme('dark')" icon="{{$darkIcon}}" icon_css="stroke-2">
+            {{$darkText}}
         </x-bladewind::dropmenu-item>
         <x-bladewind::dropmenu-item onclick="chooseTheme('system')" icon="{{$systemIcon}}" icon_css="stroke-2">
             {{$systemText}}
@@ -47,10 +47,9 @@
         const chooseTheme = (theme) => {
             theme = (theme !== undefined) ? theme : 'system';
             addToStorage('theme', theme);
-            if (theme === 'dark' || theme === 'system' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+            if (theme === 'dark' || theme === 'system') {
                 document.documentElement.classList.add('dark');
-            }
-            if (theme === 'light') {
+            } else {
                 document.documentElement.classList.remove('dark');
             }
 
@@ -59,7 +58,11 @@
             hide('.theme-system');
             unhide(`.theme-${theme}`);
         }
+        chooseTheme(getFromStorage('theme'));
 
-        chooseTheme(localStorage.theme);
+        // Listen for changes in the system theme
+        window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (event) => {
+            chooseTheme(event.matches ? 'dark' : 'light');
+        });
     </script>
 @endonce
