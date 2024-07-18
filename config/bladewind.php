443ca4556d2dc41507ff5a5e3a106ad0c6db6274
Michael K. Ocansey <kabutey@gmail.com>
2024-07-18 01:22:15 +0000

    Fix: #299

--- config/bladewind.php
+++ config/bladewind.php
@@ -158,11 +158,13 @@ return [
         'trigger_on' => 'click',
         'icon_right' => false,
         'divided' => false,
+        'padded' => true,
         // default attributes for dropmenu-item component
         'item' => [
             'dir' => '',
             'icon_right' => false,
             'hover' => true,
+            'padded' => true,
             'divided' => false,
         ]
     ],
