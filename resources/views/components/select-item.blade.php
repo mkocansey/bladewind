4e33bc2a176266590adef88ecb4a1e86a2b68b43
Marius Engen Haugen <marius.haugen@gmail.com>
2024-09-12 12:38:34 +0200

    feat: allow empty arrays in select
    Shows a non-selectable item in the dropdown

--- resources/views/components/select-item.blade.php
+++ resources/views/components/select-item.blade.php
@@ -5,18 +5,22 @@
     'flag' => '',
     'image' => '',
     'filter_by' => '',
+    'selectable' => 'true',
 ])
 @aware([ 'onselect' => '', ])
 
 @php
     $selected = filter_var($selected, FILTER_VALIDATE_BOOLEAN);
+    $selectable = filter_var($selectable, FILTER_VALIDATE_BOOLEAN);
     $label = html_entity_decode($label);
 @endphp
-<div class="py-3 pl-4 pr-3 flex items-center text-base cursor-pointer hover:bg-slate-100/90 dark:hover:bg-dark-800/50 dark:hover:text-dark-200 bw-select-item"
-     data-label="{!! $label !!}" data-value="{{ $value }}"
-     @if(!empty($filter_by)) data-filter-value="{{$filter_by}}" @endif
-     @if($selected) data-selected="true" @endif
-     @if($onselect !== '') data-user-function="{{ $onselect }}"@endif>
+<div
+        class="py-3 pl-4 pr-3 flex items-center text-base cursor-pointer bw-select-item @if($selectable) hover:bg-slate-100/90 dark:hover:bg-dark-800/50 dark:hover:text-dark-200 @else text-blue-900/40 @endif"
+        data-label="{!! $label !!}" data-value="{{ $value }}"
+        @if(!$selectable) data-unselectable @endif
+        @if(!empty($filter_by)) data-filter-value="{{$filter_by}}" @endif
+        @if($selected) data-selected="true" @endif
+        @if($onselect !== '') data-user-function="{{ $onselect }}"@endif>
     @if ($flag !== '' && $image == '')
         <i class="{{ $flag }} flag"></i>
     @endif
