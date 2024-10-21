880de6a5cd3b9dbff8c9715885454448254db564
Michael K. Ocansey <kabutey@gmail.com>
2024-10-21 08:29:02 +0000

    Increase opacity of list items divider

--- resources/views/components/list-view.blade.php
+++ resources/views/components/list-view.blade.php
@@ -8,6 +8,6 @@
     $compact = filter_var($compact, FILTER_VALIDATE_BOOLEAN);
 @endphp
 <ul role="list"
-    class="@if(!$transparent)bg-white dark:bg-transparent @endif divide-y divide-slate-100 dark:divide-dark-600/50 rounded-tl-lg rounded-tr-lg rounded-br-lg rounded-bl-lg {{$class}}">
+    class="@if(!$transparent)bg-white dark:bg-transparent @endif divide-y divide-slate-200/90 dark:divide-dark-600/50 rounded-tl-lg rounded-tr-lg rounded-br-lg rounded-bl-lg {{$class}}">
     {{ $slot }}
 </ul>
