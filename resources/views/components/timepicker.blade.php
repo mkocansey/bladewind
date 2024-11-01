01e6b25d2cacb2fb019238f752b1420c65294b52
Michael K. Ocansey <kabutey@gmail.com>
2024-11-01 04:53:54 +0000

    Cleaned up popup timepicker

--- resources/views/components/timepicker.blade.php
+++ resources/views/components/timepicker.blade.php
@@ -7,29 +7,29 @@
     'required' => 'false',
       // what should the time hours be displayed as. Available options are 12, 24
     'format' => '12',
-    'selected' => '',
-    'style' => 'icon',
+    'selected_value' => '',
+    'style' => 'popup',
     'label' => '',
     'placeholder' => 'HH:MM',
 ])
 @php
     $name = preg_replace('/[\s-]/', '_', $name);
-    if(!empty($selected)) {
-        $selected_time_array = explode(':', str_replace(' ', '', $selected));
+    if(!empty($selected_value)) {
+        $selected_time_array = explode(':', str_replace(' ', '', $selected_value));
         $selected_hour = $selected_time_array[0];
         $selected_minute = substr($selected_time_array[1], 0, 2);
         $selected_format = (strlen($selected_time_array[1]) > 2) ? strtoupper(substr($selected_time_array[1], 2, 2)) : '';
     }
 @endphp
-@if($style == 'icon')
-    <div style="width: 120px" class="flex bw-time-icon-container-{{$name}}">
+@if($style == 'popup')
+    <div style="width: 120px" class="inline-flex bw-time-icon-container-{{$name}}">
         <x-bladewind::input
                 class="bw-time-{{$name}}"
                 :name="$name"
                 suffix="clock"
                 :required="$required"
                 suffix_is_icon="true"
-                :selected_value="$selected"
+                :selected_value="$selected_value"
                 :placeholder="$placeholder"
                 :label="$label"/>
     </div>
@@ -51,6 +51,7 @@
         <div class="flex justify-center pt-4 pb-3">
             <div>
                 <x-bladewind::input numeric="true" max="{{($format=='12' ? 12 : 23)}}"
+                                    :selected_value="$selected_hour??''"
                                     class="w-[105px] text-center border-2 border-gray-200/70 rounded-md !px-4 !py-5 text-5xl font-semibold opacity-80 bw-{{$name}}_hh"
                                     placeholder="{{$hour_label}}"
                                     oninput="setTime_{{$name}}(this.value); enforceNumericLimits('bw-{{$name}}_hh', {{($format=='12' ? 12 : 23)}}); moveToMinutes('{{$name}}')"/>
@@ -62,17 +63,18 @@
             <div>
                 <x-bladewind::input numeric="true" max="59"
                                     class="w-[105px] text-center border-2 border-gray-200/70 rounded-md !px-2 !py-5 text-5xl font-semibold opacity-80 bw-{{$name}}_mm"
+                                    :selected_value="$selected_minute??''"
                                     placeholder="{{$minute_label}}"
                                     oninput="setTime_{{$name}}(this.value); enforceNumericLimits('bw-{{$name}}_mm', 59)"
                                     maxlength="2"/>
             </div>
             @if($format  == '12')
                 <div class="pl-3 space-y-1">
-                    <div class="rounded-t-lg font-semibold cursor-pointer bg-gray-100 text-2xl px-4 py-2 hover:bg-gray-300 bw-time-format-am"
+                    <div class="rounded-t-lg font-semibold cursor-pointer text-2xl px-4 py-2 {{ (!empty($selected_format) && $selected_format == 'AM') ? 'bg-gray-500 text-white' : 'bg-gray-100 hover:bg-gray-300' }} bw-time-format-am"
                          onclick="toggleFormat('AM', '{{$name}}');setTime_{{$name}}('AM')">
                         {{ __('bladewind::timepicker.AM') }}
                     </div>
-                    <div class="rounded-b-lg font-semibold cursor-pointer bg-gray-100 text-2xl px-4 py-2 hover:bg-gray-300 bw-time-format-pm"
+                    <div class="rounded-b-lg font-semibold cursor-pointer text-2xl px-4 py-2 {{ (!empty($selected_format) && $selected_format == 'PM') ? 'bg-gray-500 text-white' : 'bg-gray-100 hover:bg-gray-300' }} bw-time-format-pm"
                          onclick="toggleFormat('PM', '{{$name}}');setTime_{{$name}}('PM')">
                         {{ __('bladewind::timepicker.PM') }}
                     </div>
@@ -154,13 +156,13 @@
                         name="{{$name}}_format"
                         :required="$required"
                         selected_value="{{$selected_format??''}}">
-                    <x-bladewind::select-item label="'{{ __('bladewind::timepicker.AM') }}" value="AM"/>
-                    <x-bladewind::select-item label="'{{ __('bladewind::timepicker.PM') }}" value="PM"/>
+                    <x-bladewind::select-item label="{{ __('bladewind::timepicker.AM') }}" value="AM"/>
+                    <x-bladewind::select-item label="{{ __('bladewind::timepicker.PM') }}" value="PM"/>
                 </x-bladewind::select>
             </div>
         @endif
     </div>
-    <input type="hidden" class="bw-time-{{$name}}" name="{{$name}}" value="{{str_replace(' ', '', $selected)}}"/>
+    <input type="hidden" class="bw-time-{{$name}}" name="{{$name}}" value="{{str_replace(' ', '', $selected_value)}}"/>
 @endif
 
 <script>
