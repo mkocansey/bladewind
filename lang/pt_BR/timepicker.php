6942d2d99e5df15d5fd48476ac000825178083a3
Alexsander Fontes <sander-369@hotmail.com>
2025-01-14 12:10:39 -0300

    feat: Add Brazilian Portuguese translations for `datepicker` and `timepicker`.

---
+++ lang/pt_BR/timepicker.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Timepicker Language Lines | Português Brasileiro
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the default strings used in the timepicker
+    |
+    */
+
+    'AM' => 'AM',
+    'PM' => 'PM',
+    'POPUP_TITLE' => 'Selecionar Hora',
+    'POPUP_OKAY' => 'Salvar',
+
+];
