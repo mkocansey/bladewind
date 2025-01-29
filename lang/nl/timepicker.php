4cfa5d6d41fb6ba823b5e8e30612873a1dadc71c
Vincenzo van Wijk <vincenzo@xpslogic.com>
2025-01-29 17:28:33 +0100

    dutch language translations

---
+++ lang/nl/timepicker.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Timepicker Language Lines | Netherlands
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the default strings used in the timepicker
+    |
+    */
+
+    'AM' => 'AM',
+    'PM' => 'PM',
+    'POPUP_TITLE' => 'Tijd selecteren',
+    'POPUP_OKAY' => 'Oké',
+
+];
